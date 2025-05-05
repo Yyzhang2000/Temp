f8942c0c94c59e994e8a8362f847564a8d73a591
YuyangZhang <yuyangzhang2025@gmail.com>
2025-05-06 01:49:29 +0800

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,2 @@
 from .misc import *
 
-
-torchrun --nproc_per_node=2 train4.py --dataset cifar10 --num-labeled 4000 --arch wideresnet --batch-size 32  --total-steps 204800   --lr 0.03 --mu 7 --lambda-u 1 --use-ema --ema-decay 0.999 --global-thresh-base 0.7 --class-conf-factor 0.1 --confusion-factor 0.05 --initial-class-threshold 0.9 --ema-decay-stats 0.9 --num-workers 4 --gpu-id 0 --out result/cifar10_4000_multiview_adaptive  
